6f497cebb152db874b9a1b78c78abbc6a6e21dd7
Brody <brodyf327@gmail.com>
2020-08-16 17:13:24 -0400

    Added port for heroku.

--- checklist.md
+++
@@ -1,4 +0,0 @@
-Check all functions
-Clean up organization
-Comment on Database Functions
-Refactor code if time
--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const connectectionString = 'postgres://localhost:5432/fitness-dev';
 
-const client = new Client(connectectionString);
+const client = new Client(process.env.DATABASE_URL || connectectionString);
 
 module.exports = {
     client,
